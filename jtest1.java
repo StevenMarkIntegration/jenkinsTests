50cccd850476432956917ba06b7cd0e7d352d2f2
StevenMark <stevenmd@outlook.com>
2018-12-14 11:27:57 +1300

    changed integer values

--- jtest1.java
+++ jtest1.java
@@ -1,8 +1,8 @@
 public class jtest1 {
 
     public static void main(String[] args) {
-        int a = 5;
-        int b = 5;
+        int a = 15;
+        int b = 15;
         int c = a + b;
         
         System.out.println(a + " + " + b + " = " + c);
